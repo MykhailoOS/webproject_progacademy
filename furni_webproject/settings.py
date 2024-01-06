247cbd1c3f2a48a48335b5ff4a8c6c0778d933fe
Misha <bdemlolp234@gmail.com>
2024-01-06 15:12:18 +0200

    edit 345

--- furni_webproject/settings.py
+++ furni_webproject/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-sodqm_!&1sx+i3#7x0te_az6@yf^-@_0j)wmv)zqttxp7=89c0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', 'now.sh']
 
